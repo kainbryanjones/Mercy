2e194383c9e225b3ab520943c1a6dc55fc5fcb74
Kain <kainbryanjones.misc@gmail.com>
2022-12-21 15:11:26 +0000

    Added Paramter Ids

---
+++ Source/ParamIds.h
@@ -0,0 +1,22 @@
+/*
+  ==============================================================================
+
+	ParamIds.h
+	Created: 20 Dec 2022 5:11:28pm
+	Author:  Kain
+
+  ==============================================================================
+*/
+
+#pragma once
+
+namespace ParamIDs
+{
+	inline constexpr auto lpfCutoff{ "lpfCutoff" };
+	inline constexpr auto lpfReso{ "lpfReso" };
+	inline constexpr auto hpfCutoff{ "hpfCutoff" };
+	inline constexpr auto hpfReso{ "hpfReso" };
+	inline constexpr auto gain{ "gain" };
+	inline constexpr auto bypass{ "bypass" };
+
+}
